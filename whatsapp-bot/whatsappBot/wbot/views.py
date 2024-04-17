660d2ab6c0495cc5087e10a5face158bb60343f3
Angel Siluma <angelsiluma.03@gmail.com>
2024-04-17 23:59:12 +0200

    Update views.py

--- whatsapp-bot/whatsappBot/wbot/views.py
+++ whatsapp-bot/whatsappBot/wbot/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from twilio.rest import Client
 from django.views.decorators.csrf import csrf_exempt
 
-account_sid = 'AC90ab1ca87c094cd6628fd02128a29d63'
-auth_token = 'd28232c935557e1b8a327a795fc06883'
+account_sid = '' 
+auth_token = '' 
 client = Client(account_sid, auth_token)
 
 # Create your views here.
